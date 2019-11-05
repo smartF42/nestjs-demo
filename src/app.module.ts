b29dba509f02957595a1b7cb01baabf64fb27629
smartF42 <fengfeng@ihealthlabs.com.cn>
2019-11-05 13:27:19 +0800

    add api

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ApiController } from './api/api.controller';
+import { ApiService } from './api/api.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, ApiController],
+  providers: [AppService, ApiService],
 })
 export class AppModule {}
