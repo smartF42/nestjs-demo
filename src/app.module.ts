77385acb47ca877871e57b9b19ae59ad03f89cb6
smartF42 <fengfeng@ihealthlabs.com.cn>
2019-11-08 10:48:28 +0800

    add user(controller/service/module)

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ApiController } from './api/api.controller';
 import { ApiService } from './api/api.service';
+import { UserController } from './user/user.controller';
+import { UserService } from './user/user.service';
+import { UserModule } from './user/user.module';
+import { ApiModule } from './api/api.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController, ApiController],
-  providers: [AppService, ApiService],
+  imports: [UserModule, ApiModule],
+  controllers: [AppController, ApiController, UserController],
+  providers: [AppService, ApiService, UserService],
 })
 export class AppModule {}
